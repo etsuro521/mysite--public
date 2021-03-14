355a2719f4ad97da00183f32aabe2e0d90e689cf
etsuro521 <75.ef.35xy@gmail.com>
2021-03-15 02:21:34 +0900

    add

--- mysite/settings.py
+++ mysite/settings.py
@@ -158,5 +158,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.googlemail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = '75.ef.35xy@gmail.com'
-EMAIL_HOST_PASSWORD = 'g299792458m'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
